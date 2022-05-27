fe99a55be2014e846b232f2873cad53df214a64d
Sofapuden <sofapudr@gmail.com>
2022-05-28 00:48:12 +0200

    removed main from event_handler

--- src/event_handler.cpp
+++ src/event_handler.cpp
@@ -7,9 +7,3 @@ void f(int x, int y) {
     std::cerr << x << " and " << y << std::endl;
 }
 
-int main() {
-	MOUSE_LEFT_CLICK event;
-	auto ptr = std::make_shared <std::function <void (int, int)>> (f);
-	event.push(ptr);
-	event.callback(3,5);
-}
