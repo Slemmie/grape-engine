3220cf6326a4696ecb132312854fe5a89a6f2c6f
Slemmie <thorvejeneriksen@gmail.com>
2022-05-27 15:25:44 +0200

    entry point :)

---
+++ src/entry_point.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace ge {
+	
+	extern void on_init();
+	
+} /// namespace ge
+
+int main() {
+	// construct main application class
+	ge::on_init();
+	// call main application class run method
+	// call main application destructor
+}
